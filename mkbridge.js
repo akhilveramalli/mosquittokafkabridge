e64a21c2cad3cf631f2ff80c0050f19205fac1a5
akhilveramalli <veeramalliakhil@gmail.com>
2023-10-11 16:59:42 +0530

    zone is and deviceid separation from payload

--- mkbridge.js
+++ mkbridge.js
@@ -2,12 +2,12 @@ const kafka = require('kafka-node');
 const mqtt = require('mqtt');
 
 // Configure MQTT and Kafka brokers
-const mqttBrokerUrl = 'mqtt://localhost:1883'; // Replace with your MQTT broker URL
-const mqttUsername = 'mqtt_Username'; // Replace with your MQTT username
-const mqttPassword = 'mwtt_password'; // Replace with your MQTT password
-const kafkaBrokerUrl = 'localhost:9092'; // Replace with your Kafka broker URL
-const mqttTopic = 'mqtt_topic'; // Replace with your MQTT topic
-const kafkaTopic = 'kafka_topic'; // Replace with your Kafka topic
+const mqttBrokerUrl = 'mqtt://api.tayrix.com:1883'; // Replace with your MQTT broker URL
+const mqttUsername = 'tayrix'; // Replace with your MQTT username
+const mqttPassword = 'zepto@2023'; // Replace with your MQTT password
+const kafkaBrokerUrl = 'api.tayrix.com:9092'; // Replace with your Kafka broker URL
+const mqttTopic = 'tayrix'; // Replace with your MQTT topic
+const kafkaTopic = 'tayrix'; // Replace with your Kafka topic
 
 // Create MQTT client
 const mqttClient = mqtt.connect(mqttBrokerUrl, {
@@ -22,7 +22,12 @@ mqttClient.on('connect', () => {
 
 mqttClient.on('message', (topic, message) => {
     // Forward the MQTT message to Kafka
-    const m='{"payload":"' +btoa( message.toString()) + '"}'//converting the mqtt message to an base 64 encoding using btoa and send to the kafka topic
+    x = JSON.parse(message.toString())
+    // console.log(x.zone.deviceid)
+    // console.log(x.zone.zoneid)
+
+    const m='{"payload":"' +btoa( message.toString()) + '","zoneid":'+ x.zone.zoneid + ', "deviceid":' +  x.zone.deviceid +'}' //converting the mqtt message to an base 64 encoding using btoa and send to the kafka topic
+    console.log(m);
     kafkaProducer.send([{ topic: kafkaTopic, messages: m }], (err, data) => {
         if (err) {
             console.error('Error sending message to Kafka:', err);
